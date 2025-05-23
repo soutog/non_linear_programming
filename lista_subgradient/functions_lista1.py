4230c8271e51916a819c77585aba92df7fbb1b5c
soutog <soutog4@gmail.com>
2025-05-23 11:38:33 -0300

    test after gurobi install

--- lista_subgradient/functions_lista1.py
+++ lista_subgradient/functions_lista1.py
@@ -1,6 +1,17 @@
 import numpy as np
 
 
+def f1(xx):
+    x = xx[0]
+    if x > 0 or x == 0:
+        fv = x
+        subgrad = 1
+    else:
+        fv = -x
+        subgrad = -1
+    return fv, np.array([subgrad])
+
+
 def f4(x):
     result = abs(x[0] - 1) + abs(x[1] - 2)
 
